f2bfeb8b03a264bcdc0b4d5277c29dbc338071f6
Djjjtest <92019836+Djjjtest@users.noreply.github.com>
2021-10-08 12:51:00 +0800

    影片列表

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,33 +1,31 @@
 class VideosController < ApplicationController
   def index
-   @videos = [
+    @videos = [
      {
        "標題": "Day1 - 你適合觀看本系列教學影片嗎？",
-       "影片網址"："https://www.youtube.com/watch?v=45BYBlfkSEo",
-       "縮圖網址"："https://i.ytimg.com/vi/45BYBlfkSEo/hqdefault.jpg",
+       "影片網址": "https://www.youtube.com/watch?v=45BYBlfkSEo",
+       "縮圖網址":　"https://i.ytimg.com/vi/45BYBlfkSEo/hqdefault.jpg",
      },
      {
-       "標題"："Day2 - 建立 LINE 官方帳號並完成基本設定",
-       "影片網址"："https://www.youtube.com/watch?v=fz8T9Y4fqGY",
-       "縮圖網址"："https://i.ytimg.com/vi/fz8T9Y4fqGY/hqdefault.jpg",
+       "標題": "Day2 - 建立 LINE 官方帳號並完成基本設定",
+       "影片網址": "https://www.youtube.com/watch?v=fz8T9Y4fqGY",
+       "縮圖網址": "https://i.ytimg.com/vi/fz8T9Y4fqGY/hqdefault.jpg",
      },
      {
-       "標題"："Day3 - LINE 官方帳號聊天模式",
-       "影片網址"："https://www.youtube.com/watch?v=tb_r8edPy-E",
-       "縮圖網址"："https://i.ytimg.com/vi/tb_r8edPy-E/hqdefault.jpg",
+       "標題": "Day3 - LINE 官方帳號聊天模式",
+       "影片網址": "https://www.youtube.com/watch?v=tb_r8edPy-E",
+       "縮圖網址": "https://i.ytimg.com/vi/tb_r8edPy-E/hqdefault.jpg",
      },
      {
-       "標題"："Day4 - LINE 官方帳號自動回應",
-       "影片網址"："https://www.youtube.com/watch?v=7EGBX3QsmhA",
-       "縮圖網址"："https://i.ytimg.com/vi/7EGBX3QsmhA/hqdefault.jpg",
+       "標題": "Day4 - LINE 官方帳號自動回應",
+       "影片網址": "https://www.youtube.com/watch?v=7EGBX3QsmhA",
+       "縮圖網址": "https://i.ytimg.com/vi/7EGBX3QsmhA/hqdefault.jpg",
      },
      {
-       "標題"："Day5 - LINE 官方帳號圖文選單",
-       "影片網址"："https://www.youtube.com/watch?v=oc7yNStIeFs",
-       "縮圖網址"："https://i.ytimg.com/vi/oc7yNStIeFs/hqdefault.jpg",
+       "標題": "Day5 - LINE 官方帳號圖文選單",
+       "影片網址": "https://www.youtube.com/watch?v=oc7yNStIeFs",
+       "縮圖網址": "https://i.ytimg.com/vi/oc7yNStIeFs/hqdefault.jpg",
      },
    ]
-
-
   end
 end
