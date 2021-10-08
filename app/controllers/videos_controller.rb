4f8caa27f2b9a2d00a02c60cf8ef5c48d6bda46a
Djjjtest <92019836+Djjjtest@users.noreply.github.com>
2021-10-08 15:14:06 +0800

    fix

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,6 +6,16 @@ class VideosController < ApplicationController
         "影片網址": "https://www.youtube.com/watch?v=45BYBlfkSEo",
         "縮圖網址": "https://i.ytimg.com/vi/45BYBlfkSEo/hqdefault.jpg",
       },
+      {
+        "標題": "Day1 - 你適合觀看本系列教學影片嗎？",
+        "影片網址": "https://www.youtube.com/watch?v=45BYBlfkSEo",
+        "縮圖網址": "https://i.ytimg.com/vi/45BYBlfkSEo/hqdefault.jpg",
+      },
+      {
+        "標題": "Day1 - 你適合觀看本系列教學影片嗎？",
+        "影片網址": "https://www.youtube.com/watch?v=45BYBlfkSEo",
+        "縮圖網址": "https://i.ytimg.com/vi/45BYBlfkSEo/hqdefault.jpg",
+      },
     ]
   end
 end
