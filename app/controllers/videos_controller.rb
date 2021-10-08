7d3d205a810cdceffa48773988887d58ae4bfc5a
Djjjtest <92019836+Djjjtest@users.noreply.github.com>
2021-10-08 16:08:48 +0800

    videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -7,14 +7,14 @@ class VideosController < ApplicationController
         "縮圖網址": "https://i.ytimg.com/vi/45BYBlfkSEo/hqdefault.jpg",
       },
       {
-        "標題": "Day1 - 你適合觀看本系列教學影片嗎？",
-        "影片網址": "https://www.youtube.com/watch?v=45BYBlfkSEo",
-        "縮圖網址": "https://i.ytimg.com/vi/45BYBlfkSEo/hqdefault.jpg",
+        "標題": "Day2 - 建立 LINE 官方帳號並完成基本設定",
+        "影片網址": "https://www.youtube.com/watch?v=fz8T9Y4fqGY",
+        "縮圖網址": "https://i.ytimg.com/vi/fz8T9Y4fqGY/hqdefault.jpg",
       },
       {
-        "標題": "Day1 - 你適合觀看本系列教學影片嗎？",
-        "影片網址": "https://www.youtube.com/watch?v=45BYBlfkSEo",
-        "縮圖網址": "https://i.ytimg.com/vi/45BYBlfkSEo/hqdefault.jpg",
+        "標題": "Day3 - LINE 官方帳號聊天模式",
+        "影片網址": "https://www.youtube.com/watch?v=tb_r8edPy-E",
+        "縮圖網址": "https://i.ytimg.com/vi/tb_r8edPy-E/hqdefault.jpg",
       },
     ]
 
